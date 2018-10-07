99f9920ab0353a7fa0d22b7396187bf96b489420
vishal <vishal4790@gmail.com>
2018-10-07 09:48:00 -0400

    reverse words in a string, one line solution

---
+++ leetcode/151. Revese Words in a string/solution1_oneline.py
@@ -0,0 +1,11 @@
+Compexity
+Time: O(N)
+Space: O(N)
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return " ".join(s.strip().split()[::-1])
