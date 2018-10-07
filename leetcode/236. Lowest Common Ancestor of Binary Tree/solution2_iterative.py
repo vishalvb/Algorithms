f77e7064c2aaaa80a62bbd9edb1007675c5822e5
vishal <vishal4790@gmail.com>
2018-10-07 12:28:02 -0400

    LCA of Binary tree, Iterative

---
+++ leetcode/236. Lowest Common Ancestor of Binary Tree/solution2_iterative.py
@@ -0,0 +1,33 @@
+
+Complexity
+
+Time: O(N), N = number of nodes in tree.
+Space: O(N), we are using set to store the the child parent relationship of all the nodes. 
+
+
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        stack = [root]
+        parent = {root: None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right: 
+                parent[node.right] = node
+                stack.append(node.right)
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+        while q not in ancestors:
+            q = parent[q]
+        return q
