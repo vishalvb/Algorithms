14010780e4662f97ea819e83c9d84a8abeb57dde
vishal <vishal4790@gmail.com>
2018-10-11 17:11:24 -0400

    subtree of another tree

---
+++ leetcode/572. Subtree of Another Tree/solution1.py
@@ -0,0 +1,33 @@
+
+
+
+
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+        if t is None:
+            return True
+        else:
+            return self.help(s, t)
+    
+    def help(self, s, t) :
+        if s is None:
+            return False
+        if self.helper(s, t):
+            return True
+        else:
+            return self.help(s.left, t) or self.help(s.right, t)
+            
+    def helper(self, s, t):
+        if s is None and t is None:
+            return True
+        elif s is None or t is None:
+            return False
+        elif s.val != t.val:
+            return False
+        else:
+            return (self.helper(s.left, t.left) and self.helper(s.right, t.right))
