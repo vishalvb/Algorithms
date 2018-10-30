4f3717393be7a8391de8eb9e4342dcedb651b72d
vishal <vishal4790@gmail.com>
2018-10-29 21:36:04 -0400

    delete node linkedlist

---
+++ cracking_the_code_interview/chapter2/q3_delete_node_linkedlist.java
@@ -0,0 +1,22 @@
+
+
+
+JAVA
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+}
+
+
+PYTHON
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
