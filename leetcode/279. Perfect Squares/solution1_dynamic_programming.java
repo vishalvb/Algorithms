6784a37ec9d76e30240078cb453bc6933e1b8b60
vishal <vishal4790@gmail.com>
2018-11-22 19:43:50 -0500

    perfect squares

---
+++ leetcode/279. Perfect Squares/solution1_dynamic_programming.java
@@ -0,0 +1,26 @@
+
+
+Time: O(N ^ 2)
+Space: O(N)
+
+
+
+class Solution {
+    public int numSquares(int n) {
+        int dp[] = new int[n+1];
+        
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 1; i <=n; ++i){
+            int j = 1;
+            int min = Integer.MAX_VALUE;
+            while(i - j*j >= 0){
+                min = Math.min(dp[i - j * j] + 1, min);
+                ++j;
+            }
+            dp[i] = min;
+        }
+        
+        return dp[n];
+    }
+}
