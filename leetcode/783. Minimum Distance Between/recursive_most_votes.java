4536c9635b959b96ef83a816cce31fcdabc22dea
vishal <vishal4790@gmail.com>
2018-12-22 11:20:03 -0500

    minimum difference between two nodes in bst

---
+++ leetcode/783. Minimum Distance Between/recursive_most_votes.java
@@ -0,0 +1,16 @@
+
+Time complexity O(N).
+Space complexity O(h), h = height of the tree, at max only h recursive calls at worst point of time. 
+
+
+class Solution {
+    Integer res = Integer.MAX_VALUE, pre = null;
+    public int minDiffInBST(TreeNode root) {
+        if (root.left != null) minDiffInBST(root.left);
+        if (pre != null) res = Math.min(res, root.val - pre);
+        pre = root.val;
+        if (root.right != null) minDiffInBST(root.right);
+        return res;
+    }
+    
+}
